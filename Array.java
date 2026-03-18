246ffcf9f82b94f840f025c46399891205fa3244
Saurav Kumar <sonukumarteg245@gmail.com>
2026-03-18 09:13:02 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -196,3 +196,28 @@ Ans-class Solution {
     }
 }    
     
+
+Q.You are given an integer array arr[]. You need to find the maximum sum of a subarray (containing at least one element) in the array arr[].
+
+Note : A subarray is a continuous part of an array.
+
+Examples:
+
+Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+Output: 11
+Explanation: The subarray [7, -1, 2, 3] has the largest sum 11.
+
+Ans-class Solution {
+    int maxSubarraySum(int[] arr) {
+        // Code here
+        int currentsum=arr[0];
+        int maxsum =arr[0];
+        for(int i=1;i<arr.length;i++){
+            currentsum= Math.max(arr[i],currentsum+arr[i]);
+            maxsum = Math.max(currentsum,maxsum);
+            
+        }
+        return maxsum;
+    }
+}
+    
