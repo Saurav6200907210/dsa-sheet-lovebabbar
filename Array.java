abd3e6642fc0f040eae1dbab20f014580fd89266
Saurav Kumar <sonukumarteg245@gmail.com>
2026-03-22 08:32:22 +0530

    add more question

--- Array.java
+++ Array.java
@@ -261,3 +261,32 @@ Ans-
         return ans;
     }
 }
+
+// one question left
+Question -
+    Given an array arr[] of size n, containing elements from the range 1 to n, and each element appears at most twice, return an array of all the integers that appears twice.
+
+Note: You can return the elements in any order but the driver code will print them in sorted order.
+
+Examples:
+
+Input: arr[] = [2, 3, 1, 2, 3]
+Output: [2, 3] 
+Explanation: 2 and 3 occur more than once in the given array.
+Input: arr[] = [3, 1, 2] 
+Output: []
+Explanation: There is no repeating element in the array, so the output is empty
+Answer-
+    class Solution {
+    public ArrayList<Integer> findDuplicates(int[] arr) {
+        // code here
+         ArrayList<Integer> list = new ArrayList<>();
+         Arrays.sort(arr);
+        for(int i=1;i<arr.length;i++){
+           if(arr[i]==arr[i-1]){
+              list.add(arr[i]);
+           }
+        }
+        return list;
+    }
+}
