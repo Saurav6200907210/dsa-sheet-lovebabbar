30eea246c4e91acb297c2a1a4e3976dfa17e81b5
Saurav Kumar <sonukumarteg245@gmail.com>
2026-03-23 20:37:41 +0530

    Implement median finding methods in Array.java
    Added a method to find the median of an array and a new problem statement for finding the median of two sorted arrays.

--- Array.java
+++ Array.java
@@ -401,6 +401,33 @@ Answer-
 
 
 
+Question-
+    Given an array arr[] of integers, calculate the median.
+
+Examples:
+
+Input: arr[] = [90, 100, 78, 89, 67]
+Output: 89
+Explanation: After sorting the array middle element is the median 
+Input: arr[] = [56, 67, 30, 79]
+Output: 61.5
+Explanation: In case of even number of elements, average of two middle elements is the median. 
+
+Answer-
+    class Solution {
+    public double findMedian(int[] arr) {
+        // Code here.
+        Arrays.sort(arr);
+        int n = arr.length;
+        if(n%2!=0){
+            return arr[n/2];
+        }
+        else{
+            double ans = (arr[n/2]+arr[(n/2)-1])/2.0;
+            return ans;
+        }
+    }
+}
 
 
 Question-
