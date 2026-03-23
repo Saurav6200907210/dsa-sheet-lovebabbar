b8e65664e63844545d4497f41b1d230f4592b7a8
Saurav Kumar <sonukumarteg245@gmail.com>
2026-03-23 20:51:57 +0530

    Add palindrome check for array elements
    Implemented a method to check if all elements in an array are palindromes.

--- Array.java
+++ Array.java
@@ -401,6 +401,63 @@ Answer-
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Question-
+    iven an array arr[] of positive integers. Return true if all the array elements are palindrome otherwise, return false.
+
+Examples:
+
+Input: arr[] = [111, 222, 333, 444, 555]
+Output: true
+Explanation:
+arr[0] = 111, which is a palindrome number.
+arr[1] = 222, which is a palindrome number.
+arr[2] = 333, which is a palindrome number.
+arr[3] = 444, which is a palindrome number.
+arr[4] = 555, which is a palindrome number.
+As all numbers are palindrome so This will return true.
+Input: arr[] = [121, 131, 20]
+Output: false
+Explanation: 20 is not a palindrome hence the output is false.
+
+Answer-
+    class Solution {
+    public static boolean isPalinArray(int[] arr) {
+        // add code here.
+        for(int i=0;i<arr.length;i++){
+            int num=arr[i];
+            int original = num;
+            int rev=0;
+            while(num>0){
+                int digit=num%10;
+                rev=rev*10+digit;
+                num= num/10;
+            }
+            if(original != rev) return false;
+        }
+        return true;
+    }
+}
+
 Question-
     Given an array arr[] of integers, calculate the median.
 
