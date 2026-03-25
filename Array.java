b022a783c31d70aaddca98f7a37c993108072ef1
Saurav Kumar <sonukumarteg245@gmail.com>
2026-03-25 08:42:04 +0530

    Implement minSwap method for array element grouping
    Added a method to calculate the minimum number of swaps required to group elements less than or equal to k in an array.

--- Array.java
+++ Array.java
@@ -420,7 +420,40 @@ Answer-
 
 
 
+Question-
+    Given an array arr and a number k. One can apply a swap operation on the array any number of times, i.e choose any two index i and j (i < j) and swap arr[i] , arr[j] . Find the minimum number of swaps required to bring all the numbers less than or equal to k together, i.e. make them a contiguous subarray.
 
+Examples :
+
+Input: arr[] = [2, 1, 5, 6, 3], k = 3
+Output: 1
+Explanation: To bring elements 2, 1, 3 together, swap index 2 with 4 (0-based indexing), i.e. element arr[2] = 5 with arr[4] = 3 such that final array will be- arr[] = [2, 1, 3, 6, 5]
+Answer-
+    class Solution {
+    int minSwap(int[] arr, int k) {
+        int good = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] <= k) good++;
+        }
+
+        int bad = 0;
+        for(int i = 0; i < good; i++){
+            if(arr[i] > k) bad++;
+        }
+
+        int ans = bad;
+
+        for(int i = 0, j = good; j < arr.length; i++, j++){
+            if(arr[i] > k) bad--;
+            if(arr[j] > k) bad++;
+
+            ans = Math.min(ans, bad);
+        }
+        return ans;
+    }
+}
+    
 Question-
     iven an array arr[] of positive integers. Return true if all the array elements are palindrome otherwise, return false.
 
