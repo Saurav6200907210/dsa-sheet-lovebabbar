bccd8f3c957ca07fc3053adf265aaa73b6d7f737
Saurav Kumar <sonukumarteg245@gmail.com>
2026-03-28 12:14:52 +0530

    Implement chocolate distribution and subarray sum questions
    Added a method to find the minimum difference in chocolates distributed among students and a question regarding finding the smallest subarray with a sum greater than a given value.

--- Array.java
+++ Array.java
@@ -413,8 +413,35 @@ Answer-
 
 
 
+Questions-
+    Given an array arr[] of positive integers, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m students, the task is to distribute chocolate packets among m students such that -
+      i. Each student gets exactly one packet.
+     ii. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum and return that minimum possible difference.
 
+Examples:
 
+Input: arr = [3, 4, 1, 9, 56, 7, 9, 12], m = 5
+Output: 6
+Explanation: The minimum difference between maximum chocolates and minimum chocolates is 9 - 3 = 6 by choosing following m packets :[3, 4, 9, 7, 9].
+Input: arr = [7, 3, 2, 4, 9, 12, 56], m = 3
+Output: 2
+Explanation: The minimum difference between maximum chocolates and minimum chocolates is 4 - 2 = 2 by choosing following m packets :[3, 2, 4].
+
+Answer-
+    class Solution {
+    public int findMinDiff(ArrayList<Integer> arr, int m) {
+        // your code here
+        int n = arr.size(); 
+        if (m > n) return -1;
+        Collections.sort(arr);
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<=n-m;i++){
+        int diff=arr.get(i+m-1)-arr.get(i);
+            ans=Math.min(ans,diff);
+        }
+        return ans;
+    }
+}
 
 Question-
 
