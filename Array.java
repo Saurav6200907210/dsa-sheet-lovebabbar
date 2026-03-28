9a24d583029b3266e06c379f3fc72ec331dc20ba
Saurav Kumar <sonukumarteg245@gmail.com>
2026-03-28 15:54:50 +0530

    Implement water trapping and chocolate distribution problems
    Added a solution to calculate trapped rainwater and a problem statement for chocolate distribution.

--- Array.java
+++ Array.java
@@ -411,7 +411,44 @@ Answer-
 
 
 
+Question-
+    Given an array arr[] with non-negative integers representing the height of blocks. If the width of each block is 1, compute how much water can be trapped between the blocks during the rainy season. 
 
+Examples:
+
+Input: arr[] = [3, 0, 1, 0, 4, 0, 2]
+Output: 10
+Explanation: Total water trapped = 0 + 3 + 2 + 3 + 0 + 2 + 0 = 10 units.
+Answer-
+    class Solution {
+    public int maxWater(int arr[]) {
+        int n = arr.length;
+        if (n == 0) return 0;
+
+        int[] leftmax = new int[n];
+        int[] rightmax = new int[n];
+
+        // left max
+        leftmax[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            leftmax[i] = Math.max(leftmax[i - 1], arr[i]);
+        }
+
+        // right max
+        rightmax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightmax[i] = Math.max(rightmax[i + 1], arr[i]);
+        }
+
+        // water calculate
+        int water = 0;
+        for (int i = 0; i < n; i++) {
+            water += Math.min(leftmax[i], rightmax[i]) - arr[i];
+        }
+
+        return water;
+    }
+}
 
 Questions-
     Given an array arr[] of positive integers, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m students, the task is to distribute chocolate packets among m students such that -
