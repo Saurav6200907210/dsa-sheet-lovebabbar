252e997952083cd606a6f9840f022944577b66c1
Saurav Kumar <sonukumarteg245@gmail.com>
2026-03-31 06:55:17 +0530

    Add searchMatrix method for 2D matrix search
    Implement binary search to find a number in a sorted 2D matrix.

--- matrix.java
+++ matrix.java
@@ -54,3 +54,45 @@ Answerr-
     }
 }
 
+
+
+Question 2-
+Given a strictly sorted 2D matrix mat[][] of size n x m and a number x. Find whether the number x is present in the matrix or not.
+Note: In a strictly sorted matrix, each row is sorted in strictly increasing order, and the first element of the ith row (i!=0) is greater than the last element of the (i-1)th row.
+
+Examples:
+
+Input: mat[][] = [[1, 5, 9], [14, 20, 21], [30, 34, 43]], x = 14
+Output: true
+Explanation: 14 is present in the matrix, so output is true.
+Input: mat[][] = [[1, 5, 9, 11], [14, 20, 21, 26], [30, 34, 43, 50]], x = 42
+Output: false
+Explanation: 42 is not present in the matrix.
+
+ Answer -
+ class Solution {
+    public boolean searchMatrix(int[][] mat, int x) {
+        // code here
+        int n=mat.length;
+        int m=mat[0].length;
+        int low= 0;
+        int high= n*m -1;
+        
+        while(low<= high){
+            int mid=(low+high)/2;
+            int row=mid/m;
+            int col= mid%m;
+            if(mat[row][col] == x){
+                return true;
+            }
+            else if(mat[row][col]<x){
+                low=mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return false;
+    }
+}
+ 
